07a793346376e749bc3bdac2495439037cd02c88
Mariana-Coelho <129106508+Mariana-Coelho@users.noreply.github.com>
2023-08-21 12:18:27 -0300

    3_Bimestre_Mariana de Lourdes Coelho
    Atividade Mariana de Lourdes Coelho

--- lib/componentes/Item_Categoria.dart
+++ lib/componentes/Item_Categoria.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:navegacao/models/categorias.dart';
+import 'package:navegacao/telas/tela_produtos.dart';
 
 class ItemCategoria extends StatelessWidget{
   final Categoria categoria;
-  ItemCategoria(this.categoria);
+  ItemCategoria(this.categoria); 
+
+    void selecionarCategoria(BuildContext context){
+Navigator.of(context).push(
+  MaterialPageRoute(builder: (_){
+    return TelaProdutos(categoria);
+  })
+);
+    }
+
   @override 
-  Widget build(BuildContext){
-    return Container(
-      child: Text(categoria.titulo),
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            categoria.color.withOpacity(0.5),
-            categoria.color
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
+  Widget build(BuildContext context){
+    return InkWell(
+      onTap: () => selecionarCategoria(context),
+      child: Container(
+        child: Text(categoria.titulo),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              categoria.color.withOpacity(0.5),
+              categoria.color
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          )
         )
-      )
+      ),
     );
   }
 }
---
+++ lib/telas/tela_produtos.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao/models/categorias.dart';
+
+class TelaProdutos extends StatelessWidget {
+  final Categoria categoria;
+
+  TelaProdutos(this.categoria);
+
+  // Exemplo de lista de pratos relacionados à categoria
+  List<Prato> pratosDaCategoria = [
+    Prato("Lasanha", "Uma deliciosa lasanha de carne com molho de tomate e queijo derretido.", "lasanha.jpg"),
+    Prato("Strogonoff", "Strogonoff de frango com molho cremoso e arroz branco.", "strogonoff.jpg"),
+    Prato("Batata frita", "Batatas fritas crocantes e douradas, perfeitas para petiscar.", "batata_frita.jpg"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Produtos"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text("A categoria escolhida foi ${categoria.titulo}", style: TextStyle(fontSize: 18)),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: pratosDaCategoria.length,
+              itemBuilder: (ctx, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Card(
+                    child: ListTile(
+                      title: Text(pratosDaCategoria[index].nome),
+                      subtitle: Text(pratosDaCategoria[index].descricao),
+                      leading: Image.asset(
+                        pratosDaCategoria[index].caminhoImagem,
+                        width: 70, // ajuste o tamanho da imagem conforme necessário
+                        height: 70,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Modelo de Prato (substitua pelas suas classes/modelos reais)
+class Prato {
+  final String nome;
+  final String descricao;
+  final String caminhoImagem;
+
+  Prato(this.nome, this.descricao, this.caminhoImagem);
+}
